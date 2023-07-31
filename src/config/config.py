f9fe2720b93d3d59dc645123926af8ff5cd3e57e
Yacine <y.driss@esi-sba.dz>
2023-07-31 14:42:22 +0100

    add .gitignore file

--- src/config/config.py
+++ src/config/config.py
@@ -1,7 +1,7 @@
 import os
 
 
-_data_path = os.path.join(os.getcwd(),'src','ressources','data')
+_data_path = os.path.join(os.getcwd(), 'src','ressources','data')
 _asset_path = os.path.join(os.getcwd(),'src','ressources','assets')
 
 scaler_path = os.path.join(_asset_path,'scaler.pkl')
